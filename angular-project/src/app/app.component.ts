ecc19a8efdc15284096a7750c6ec1d06281ccc38
Michael Torres <michaelj.torres@yahoo.com>
2019-02-26 17:18:27 -0500

    angular toggle assignment

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  username = '';
+buttonPress = false;
+
+
+ ngOnInit() {
+
+ }
+ tap(){
+   this.buttonPress = true;
+ }
 }
